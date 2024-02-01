b3de85121382cc1b929ac07a288645570dce6e18
Artur Galiullin <Artur.Galiullin@softline.com>
2024-02-01 16:46:40 +0300

    initial commit

---
+++ webpack.config.ts
@@ -0,0 +1,23 @@
+import path from 'path';
+import { Configuration } from 'webpack';
+
+import { BuildEnv, BuildMode, BuildPaths } from './config/build/types/config';
+import { buildWebpackConfig } from './config/build/buildWebpackConfig';
+
+export default (env: BuildEnv): Configuration => {
+	const paths: BuildPaths = {
+		entry: path.resolve(__dirname, 'src', 'index.tsx'),
+		output: path.resolve(__dirname, 'build'),
+		html: path.resolve(__dirname, 'public', 'index.html'),
+	};
+	const mode: BuildMode = env.mode || 'development';
+	const isDev = mode === 'development';
+	const port = env.port || 8080;
+
+	return buildWebpackConfig({
+		mode,
+		paths,
+		isDev,
+		port,
+	});
+};
