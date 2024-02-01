f96bb9fa24bec2da30c320ac9057056a4eda5df9
Artur Galiullin <Artur.Galiullin@softline.com>
2024-02-01 17:01:47 +0300

    add config

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,18 @@
+import { ProgressPlugin, WebpackPluginInstance } from 'webpack';
+import HTMLWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+
+import { BuildOptions } from './types/config';
+
+export function buildPlugins({ paths }: BuildOptions): WebpackPluginInstance[] {
+	return [
+		new ProgressPlugin(),
+		new HTMLWebpackPlugin({
+			template: paths.html,
+		}),
+		new MiniCssExtractPlugin({
+			filename: 'css/[name].[contenthash:8].css',
+			chunkFilename: 'css/[name].[contenthash:8].css',
+		}),
+	];
+}
