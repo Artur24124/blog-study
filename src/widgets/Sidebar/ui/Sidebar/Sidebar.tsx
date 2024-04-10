fe8b2958c25a46910c9c4f9ab58f907d0f09b6e2
Artur Galiullin <Artur.Galiullin@softline.com>
2024-04-10 17:01:11 +0300

    hide toggle button

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -27,12 +27,12 @@ export const Sidebar: FC<SidebarProps> = ({ className }) => {
 				[className],
 			)}
 		>
-			<Button
+			{/* <Button
 				data-testid='sidebar-toggle'
 				onClick={onToggle}
 			>
 				toggle
-			</Button>
+			</Button> */}
 			<div className={cls.switchers}>
 				<ThemeSwitcher />
 				<LangSwitcher />
