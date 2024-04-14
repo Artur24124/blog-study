eb1777cf0c3ff9469a1553791e938a7b5b9ad45f
Artur Galiullin <Artur.Galiullin@softline.com>
2024-04-14 20:52:20 +0300

    fixed tests

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,16 +1,16 @@
 import { fireEvent, screen } from '@testing-library/react';
 
-import { renderWithTranslation } from 'shared/lib/tests';
+import { componentRender } from 'shared/config/tests';
 import { Sidebar } from './Sidebar';
 
 describe('Sidebar', () => {
 	test('render Sidebar', () => {
-		renderWithTranslation(<Sidebar />);
+		componentRender(<Sidebar />);
 		expect(screen.getByTestId('sidebar')).toBeInTheDocument();
 	});
 
 	test('toggle Sidebar', () => {
-		renderWithTranslation(<Sidebar />);
+		componentRender(<Sidebar />, { route: '/' });
 		const toggleBtn = screen.getByTestId('sidebar-toggle');
 		expect(screen.getByTestId('sidebar')).toBeInTheDocument();
 		fireEvent.click(toggleBtn);
